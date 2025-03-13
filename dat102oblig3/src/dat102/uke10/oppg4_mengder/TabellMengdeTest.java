ee531ce369ee5818092560a60e999bf177e847ae
MariusBorgen <186505@stud.hvl.no>
2025-03-13 13:13:32 +0100

    Start

---
+++ dat102oblig3/src/dat102/uke10/oppg4_mengder/TabellMengdeTest.java
@@ -0,0 +1,14 @@
+package dat102.uke10.oppg4_mengder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TabellMengdeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- dat102oblig3/src/oppgave/oppgave4.java
+++
@@ -1,5 +0,0 @@
-package oppgave;
-
-class oppgave4 {
-
-}
