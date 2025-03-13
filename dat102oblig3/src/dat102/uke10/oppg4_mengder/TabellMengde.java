6922787ae6339ddfe79e5d9e64bf11e6c3f5a64f
MariusBorgen <186505@stud.hvl.no>
2025-03-13 15:05:41 +0100

    TabellMengde

--- dat102oblig3/src/dat102/uke10/oppg4_mengder/TabellMengde.java
+++ dat102oblig3/src/dat102/uke10/oppg4_mengder/TabellMengde.java
@@ -1,96 +1,46 @@
 package dat102.uke10.oppg4_mengder;
 
 public class TabellMengde<T> {
-
-/**
-	 * En implementasjon av BagADT
-	 * 
-	 * @author Lars-Petter Helland
-	 */
-		private static final int DEFAULT_KAPASITET = 10;
-
-		private T[] tabell;
-		private int antall;
-
-		/************************************************************/
-
-		public TabellMengde() {
-			this(DEFAULT_KAPASITET);
-		}
-
-		@SuppressWarnings("unchecked")
-		public TabellMengde(int kapasitet) {
-			tabell = (T[]) new Object[kapasitet];
-			antall = 0;
-		}
-
-		public int getCurrentSize() {
-			return antall;
-		}
-
-		public boolean isEmpty() {
-			return antall == 0;
-		}
-
-		private boolean erFull() {
-			return antall == tabell.length;
-		}
+	
+//	Sjekk om tabellen har null eller ingen elementer.   
+	
+	public static <T> boolean sokTabellTom(T[] tabell, T element) {
 		
-		public boolean add(T newEntry) {
-			
-			if (erFull()) {
-				return false;
+	for (T tabel:tabell) {
+			if (tabel.equals(null)) {
+				return true;
 			}
-			
-			tabell[antall] = newEntry;
-			antall++;
-			return true;
-		}
+		}		
+		return false;
+}
 
-		public T remove() {
-			
-			if (isEmpty()) {
-				return null;
-			}
-			
-			T element = tabell[antall-1];
-			tabell[antall-1] = null;
-			antall--;
-			return element;
-		}
+	public static <T> boolean sokTabell(T[] tabell, T element) {
 
-		public boolean remove(T anEntry) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public void clear() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public int getFrequencyOf(T anEntry) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		public boolean contains(T anEntry) {
-			
-			for (int i = 0; i < antall; i++) {
-				if (tabell[i].equals(anEntry)) {
+//		Løkke gjennom tabellen og sjekk likhet (`equals`). 
+		
+		for (T tabel:tabell) {
+				if (tabel.equals(element)) {
 					return true;
 				}
-			}
+			}		
 			return false;
-		}
-
-		public T[] toArray() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		/************************************************************/
-
 	}
+}
+		/*
+	                          
+	Gå gjennom elementene og sjekk om de finnes i den andre mengden.             
+	Sammenlign lengde og sjekk at alle elementer er like.                        
+	Sjekk at ingen elementer finnes i begge mengder.
+	Gå gjennom begge mengdene og legg felles elementer i en ny mengde.
+	Kombiner alle elementer fra begge mengder i en ny mengde.                    
+	Fjern alle elementer i den andre mengden fra den første.                     
+	Sjekk at elementet ikke finnes før du legger det til i tabellen.             
+	Finn elementet og fjern det. Flytt etterfølgende elementer ett hakk bakover.
+	Returner en kopi av tabellen med alle elementene.             
+	Returner lengden på tabellen.
+	*/
+	
+
+
 
 
--- dat102oblig3/src/dat102/uke10/oppg4_mengder/TabellMengdeTest.java
+++ dat102oblig3/src/dat102/uke10/oppg4_mengder/TabellMengdeTest.java
@@ -1,14 +1,38 @@
 package dat102.uke10.oppg4_mengder;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.classfile.components.ClassPrinter.Node;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class TabellMengdeTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+/* --- Data brukt i testene -------------------------------------- */ 
+	
+	static Integer[] tomTabell = {};
+	static Integer[] Tabell = new Integer[] { 2, 8, 0, 4, 6 };
 
+	/* --------------------------------------------------------------- */
+	
+	//TODO Vi må jo skrive tester for å sjekke at det vi lager virker !!
+	
+	
+	
+
+		@Test
+		void sokTabellTomTest() {
+			assertFalse(TabellMengde.sokTabell(tomTabell,0));
+		}
+		
+		void sokTabellTest() {
+			assertTrue(TabellMengde.sokTabell(Tabell,2));
+			assertTrue(TabellMengde.sokTabell(Tabell,0));
+			assertTrue(TabellMengde.sokTabell(Tabell,6));
+			assertFalse(TabellMengde.sokTabell(Tabell,5));
+
+		}
 }
